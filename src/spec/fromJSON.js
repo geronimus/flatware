38d574835fca9016652a649342c706d74c708004
Graham Brown (Geronimus) <leo.orientis@outlook.com>
2019-04-02 01:07:23 -0400

    Factor 30 lines out of the specFromJSON implementation

--- src/spec/fromJSON.js
+++ src/spec/fromJSON.js
@@ -8,7 +8,7 @@ function specFromJSON( jsonObj ) {
   
   let obj;
 
-  try { obj = rehydrateDates( JSON.parse( jsonObj ) ); }
+  try { obj = JSON.parse( jsonObj, reviveDates ); }
   catch( error ) {
     IllegalArgument(
       "jsonObj",
@@ -19,33 +19,14 @@ function specFromJSON( jsonObj ) {
 
   return specFromObject( obj );
 
-  function rehydrateDates( obj ) {
-    
-    Object.keys( obj ).forEach( setting => {
-      if ( obj[ setting ][ "type" ] === "Date" )
-        Object.keys( obj[ setting ] )
-          .filter( item => [ "upperBound", "lowerBound", "optionsList" ].includes( item ) )
-          .forEach( item => {
-            if ( item === "optionsList" && Array.isArray( obj[ setting ][ item ] ) )
-              obj[ setting ][ item ] = obj[ setting ][ item ].map( isoString => getDateVal( isoString ) );
-            else
-              obj[ setting ][ item ] = getDateVal( obj[ setting ][ item ] );
-          });
-    });
+  function reviveDates( key, val ) {
+    const isoDatePattern =
+      /^([+-]\d{2})?\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d{1,3}Z$/;
 
-    return obj;
-
-    function getDateVal( isoString ) {
-      const isoDatePattern = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/;
-
-      if (
-        typeof isoString === "string" &&
-          isoDatePattern.test( isoString )
-      )
-        return new Date( isoString );
-      else
-        return isoString;
-    }
+    if ( typeof val === "string" && isoDatePattern.test( val ) )
+      return new Date( val );
+    else
+      return val;
   }
 }
 
--- test/spec/fromJSON.test.js
+++ test/spec/fromJSON.test.js
@@ -53,7 +53,6 @@ describe( "specFromJSON( jsonObj )", () => {
             );
           });
       });
-
   });
 });
 
