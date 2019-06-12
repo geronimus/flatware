af6c127be66a8b6eafc0f036d6a32e09d92cb5ef
Graham Brown (Geronimus) <leo.orientis@outlook.com>
2019-06-11 23:36:33 -0400

    Fix publish script

--- scripts/publish-npm-artifacts.js
+++ scripts/publish-npm-artifacts.js
@@ -5,26 +5,27 @@ const fs = require( "fs" );
 const licence = getSourcePath( "LICENCE" );
 const readme = getSourcePath( "README.md" );
 const srcPackage = getSourcePath( "package.json" );
-const dist = getSourcePath( "dist" );
-const publishedPackage = path.resolve( dist, "package.json" );
+const publishedLicence = getSourcePath( "./dist/LICENCE" );
+const publishedReadme = getSourcePath( "./dist/README.md" );
+const publishedPackage = getSourcePath( "./dist/package.json" );
 
 // Procedure
 fs.copyFile(
   licence,
-  dist,
-  ( err ) => { reportResult( "copy", licence, dist, err ); }
+  publishedLicence,
+  ( err ) => { reportResult( "copy", licence, publishedLicence, err ); }
 );
 
 fs.copyFile(
   readme,
-  dist,
-  ( err ) => { reportResult( "copy", readme, dist, err ); }
+  publishedReadme,
+  ( err ) => { reportResult( "copy", readme, publishedReadme, err ); }
 );
 
 fs.readFile(
   srcPackage,
   ( err, data ) => {
-    if ( err !== undefined )
+    if ( err !== undefined && err !== null )
       reportResult( "publish", srcPackage, publishedPackage, err );
     else
       publishPackage( makePublishedPackage( JSON.parse( data ) ) );
@@ -33,13 +34,13 @@ fs.readFile(
 
 // Supporting Functions
 function getSourcePath( sourceRelativeName ) {
-  return path.resolve( __dirname, sourceRelativeName );
+  return path.resolve( __dirname, "../", sourceRelativeName );
 }
 
 function reportResult( op, source, destination, err ) {
-  if ( err !== undefined ) {
-    console.log( `Could not ${ op } ${ source } to ${ destination }\n` );
-    colsole.log( `Error:\n${ err }` );
+  if ( err !== undefined && err !== null  ) {
+    console.log( `Could not ${ op } ${ source } to ${ destination }` );
+    console.log( `Error:\n${ err }\n` );
   } else
     console.log( `Success:\n${ op } ${ source } to ${ destination }` );
 }
